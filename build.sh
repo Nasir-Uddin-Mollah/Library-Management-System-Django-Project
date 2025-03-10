a3acab4bddf369b66a399edd553645417051f66f
Nasir <nasirhossenmollah@gmail.com>
2025-03-10 23:10:49 +0600

    added build.sh and requirements.txt

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
